54bf1b7238a6bf5ac028f38592f1eb8b42102d50
Seungshin Lee <lsver67@gmail.com>
2022-07-02 10:12:31 +0900

    구인구직 크롤링 기능 완성

--- CNU_JobOpening/CNU_JobOpening.py
+++ CNU_JobOpening/CNU_JobOpening.py
@@ -1,52 +1,56 @@
+from urllib import response
 from bs4 import BeautifulSoup
 import requests
-from datetime import date
+from datetime import datetime
+import time
 
+# 오늘날짜 불러오기
+currentDay = datetime.now().date()
+HTMLdatetimeFormat = "%Y.%m.%d"
+print(currentDay)
 
-##############################
-#디버그 모드 설정
-DEBUG = True
+# 정보가 최신인지 확인
+isNew = True
 
-###############################
-if DEBUG:
-    import time
-    startTime = time.time()
+# 웹 페이지 5페이지 까지 검색
+for page in range(1,5):
 
-#웹페이지 불러오기
-newboard_html = requests.get("https://plus.cnu.ac.kr/_prog/recruit/?menu_dvs_cd=07080401&site_dvs_cd=kr")
-#웹페이지 파싱
-newboard_soup = BeautifulSoup(newboard_html.content.decode('utf-8','replace'), 'html.parser')
+    # 접속 주소
+    url = "https://plus.cnu.ac.kr/_prog/recruit/?site_dvs_cd=kr&menu_dvs_cd=07080401&gubun=1&skey=&sval=&GotoPage="+str(page)
+    tr = []
+    response = requests.get(url)
 
-#파싱작업 끝난 데이터 필요한 부분만 추출
-newboard = newboard_soup.find('div' , {'class':'tb_wrap'})
-newboard_list = newboard.find('table', {'class':'default_table'})
+    print("PAGE",page)
 
+    # 만약 웹페이지가 정상적으로 불러왔으면
+    if response.status_code == 200:
 
-print(newboard_list.text)
-print("\n")
+        html = response
+        soup = BeautifulSoup(html.content.decode('utf-8','replace'), 'html.parser')
 
-##웹페이지 불러오기
-#academicInfo_html = requests.get("https://plus.cnu.ac.kr/_prog/_board/?code=sub07_0702&site_dvs_cd=kr&menu_dvs_cd=0702")
-##웹페이지 파싱
-#academicInfo_soup = BeautifulSoup(academicInfo_html.content.decode('utf-8','replace'), 'html.parser')
-#
-##파싱작업 끝난 데이터 필요한 부분만 추출
-#academicInfo = academicInfo_soup.find('div' , {'class':'board_list'})
-#academicInfo_list = academicInfo.find_all('td', {'class':'title'})
-#
-#for i in range(0,len(academicInfo_list)):
-#    print(academicInfo_list[i].text)
-#
-#print("\n")
-#print(date.today())
-##웹페이지 불러오기
-#eduInfo_html = requests.get("https://plus.cnu.ac.kr/_prog/_board/?code=sub07_0704&site_dvs_cd=kr&menu_dvs_cd=0704")
-##웹페이지 파싱
-#eduInfo_soup = BeautifulSoup(eduInfo_html.content.decode('utf-8','replace'), 'html.parser')
-#
-##파싱작업 끝난 데이터 필요한 부분만 추출
-#eduInfo = eduInfo_soup.find('div' , {'class':'board_list'})
-#eduInfo_list = eduInfo.find_all('td', {'class':'title'})
-#eduInfo_date = eduInfo.find_all('td',{'class','date'})
-#for i in range(0,len(eduInfo_list)):
-#    print(eduInfo_list[i].text,"  ",eduInfo_date[i].text)
+        # 구인구직 목록 접근
+        for i in range(1,11):
+            selector = '#txt > div.tb_wrap > table > tbody > tr:nth-child(' + str(i) + ')'
+            job = soup.select_one(selector)
+            dates = datetime.strptime(job.find('td',{'class':'data'}).text, HTMLdatetimeFormat).date()
+
+            # 만약 게시글 날짜가 오늘이면 실행, 아니면 정지
+            if dates == currentDay:
+                isNew = True
+            else:
+                isNew = False
+                break
+
+            num = job.find('td',{'class':'num'}).text
+            title = job.find('td',{'class':'title'}).text
+            center = job.find('td',{'class':'center'}).text
+            print(num,title,center,dates)
+
+    else:
+        print(response.status_code)
+
+    # 게시글이 오늘날짜가 아니면 5분간 정지
+    if(not isNew):
+        print("No New Info, Sleeping...")
+        isNew = True
+        time.sleep(300)
